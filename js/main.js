7f772787465829e59ae1f4ce73ca01949d5db40f
Andy Blount <AndyBlount@picnic.Concrete.local>
2013-07-16 14:25:23 +0100

    wireframe work

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		$.ajax({
 			url: remote,
 			context: document.body,
-			success: function(){
+			success: function(data){
 				//when Successfully executed
 				$('#myModal .modal-body').hmtl(data);
 				//$('#myModal').modal('show');
